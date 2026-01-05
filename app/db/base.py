ad7874e4f04975beb08cfea8452e2a4fddba1c31
Allen <54683515+awzhao1@users.noreply.github.com>
2026-01-05 16:22:54 -0500

    Initialize Alembic to create tables and test and bugfix

--- app/db/base.py
+++ app/db/base.py
@@ -3,4 +3,4 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 # Import models here so SQLAlchemy knows about them
-from app.db import models
+from app.db.models import ticket
